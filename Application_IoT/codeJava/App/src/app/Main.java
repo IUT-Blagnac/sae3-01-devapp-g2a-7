74f257b14e616d78c4dbd110e45ce9c9f6e9aee4
Tilian <onsenfou>
2023-01-05 12:28:19 +0100

    implémentation interface JavaFX

--- Application_IoT/codeJava/App/src/app/Main.java
+++ Application_IoT/codeJava/App/src/app/Main.java
@@ -1,11 +1,15 @@
 package app;
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -14,8 +18,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("./controller/mainView.fxml"));
-            BorderPane root = loader.load();
+            loader.setLocation(getClass().getResource("controller/mainView.fxml"));
+            SplitPane root = loader.load();
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -24,4 +28,5 @@ public class Main extends Application {
             System.exit(1);
         }
     }
+
 }
--- Application_IoT/codeJava/App/src/app/controller/mainController.java
+++ Application_IoT/codeJava/App/src/app/controller/mainController.java
@@ -1,15 +1,19 @@
 package app.controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.StackedBarChart;
+
 
 public class mainController implements Initializable  {
 
+    @FXML
+    StackedBarChart barChart;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
-    }
+    public void initialize(URL location, ResourceBundle resources) {}
     
 }

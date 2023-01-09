2ce41c91a5a3ba9611d05dd17d26b448c454a097
Tilian <onsenfou>
2023-01-09 15:20:36 +0100

    update interface JavaFX (Modif controller)

--- Application_IoT/codeJava/App/src/app/controller/mainController.java
+++ Application_IoT/codeJava/App/src/app/controller/mainController.java
@@ -15,32 +15,90 @@ import javafx.scene.control.SpinnerValueFactory;
 public class mainController implements Initializable  {
 
     private ArrayList<Spinner<Double>> spinners = new ArrayList<Spinner<Double>>();
+    private ArrayList<StackedBarChart> barCharts = new ArrayList<StackedBarChart>();
 
-    @FXML
-    StackedBarChart<String, Float> barChart;
     @FXML
     CheckBox CBActivite;
     @FXML
     Spinner<Double> spinnerActivite;
     @FXML
+    StackedBarChart<String, Float> BCActivite;
+    @FXML
     CheckBox CBCO2;
     @FXML
     Spinner<Double> spinnerCO2;
+    @FXML
+    StackedBarChart<String, Float> BCCO2;
+    @FXML
+    CheckBox CBHumidite;
+    @FXML
+    Spinner<Double> spinnerHumidite;
+    @FXML
+    StackedBarChart<String, Float> BCHumidite;
+    @FXML
+    CheckBox CBIllumination;
+    @FXML
+    Spinner<Double> spinnerIllumination;
+    @FXML
+    StackedBarChart<String, Float> BCIllumination;
+    @FXML
+    CheckBox CBInfrarouges;
+    @FXML
+    Spinner<Double> spinnerInfrarouges;
+    @FXML
+    StackedBarChart<String, Float> BCInfrarouges;
+    @FXML
+    CheckBox CBPression;
+    @FXML
+    Spinner<Double> spinnerPression;
+    @FXML
+    StackedBarChart<String, Float> BCPression;
+    @FXML
+    CheckBox CBTemperature;
+    @FXML
+    Spinner<Double> spinnerTemperature;
+    @FXML
+    StackedBarChart<String, Float> BCTemperature;
+    @FXML
+    CheckBox CBQualiteAir;
+    @FXML
+    Spinner<Double> spinnerQualiteAir;
+    @FXML
+    StackedBarChart<String, Float> BCQualiteAir;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Liste des spinners
         spinners.add(spinnerActivite);
         spinners.add(spinnerCO2);
+        spinners.add(spinnerHumidite);
+        spinners.add(spinnerIllumination);
+        spinners.add(spinnerInfrarouges);
+        spinners.add(spinnerPression);
+        spinners.add(spinnerTemperature);
+        spinners.add(spinnerQualiteAir);
+        // Initialisation des spinners
         for (Spinner<Double> spinner : spinners) {
             spinner.setValueFactory(
                 new SpinnerValueFactory.DoubleSpinnerValueFactory(-9999, 9999, 0, 0.1)
             );
             // Empêche la saisie de valeurs non-numériques
             spinner.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("-{0,1}\\d*(\\.|,){0,1}\\d*")) {
-                    spinner.getEditor().setText(oldValue);
-            }});
+                spinner.getEditor().setText((
+                    newValue.matches("-{0,1}\\d*(\\.|,){0,1}\\d*") ?
+                        newValue.replace(".", ",") : oldValue
+                ));
+            });
         }
+        // Liste des barCharts
+        barCharts.add(BCActivite);
+        barCharts.add(BCCO2);
+        barCharts.add(BCHumidite);
+        barCharts.add(BCIllumination);
+        barCharts.add(BCInfrarouges);
+        barCharts.add(BCPression);
+        barCharts.add(BCTemperature);
+        barCharts.add(BCQualiteAir);
     }
     
 }

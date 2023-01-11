87c7ddba50d7a3a2a66873e26b2a958051c5ba2d
Tilian HURÉ <tilian.contact@gmail.com>
2023-01-11 19:43:41 +0100

    maj

--- siteWeb/pages/connexion.php
+++ siteWeb/pages/connexion.php
@@ -3,8 +3,12 @@
     include("../include/infoPopup.php");
 
     // Affiche un message de confirmation lorsqu'un compte client a été créé
-    if (isset($_GET["nouveauCompte"]) && $_GET["nouveauCompte"] == "true") {
-        echo '<script type="text/javascript">show_info_popup("Votre compte a bien été créé, vous pouvez maintenant vous connecter.", "var(--green-blue)")</script>';
+    if (isset($_GET["messageInfo"])) {
+        if ($_GET["messageInfo"] == "compteCree") {
+            echo '<script type="text/javascript">show_info_popup("Votre compte a bien été créé, vous pouvez maintenant vous connecter.", "var(--green-blue)")</script>';
+        } else if ($_GET["messageInfo"] == "compteModifie") {
+            echo '<script type="text/javascript">show_info_popup("Votre compte a bien été modifié, vous pouvez maintenant vous re-connecter.", "var(--green-blue)")</script>';
+        }
     }
 
     // Processus de connexion
@@ -61,6 +65,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../public/css/style.css">
         <link rel="stylesheet" href="../public/css/header.css">
+        <link rel="stylesheet" href="../public/css/footer.css">
         <link rel="stylesheet" href="../public/css/connexionStyle.css">
         <title>Connexion</title>
     </head>
@@ -71,8 +76,8 @@
                 <h1>J'ai déjà un compte</h1>
                 <form id="form-connexion" method="post">
                     <p>Entrez vos identifiants pour vous connecter :</p>
-                    <input type="email" name="mail" placeholder="Adresse e-mail">
-                    <input type="password" name="mdp" placeholder="Mot de passe">
+                    <input type="email" name="mail" placeholder="Adresse e-mail" required>
+                    <input type="password" name="mdp" placeholder="Mot de passe" required>
                     <a href="#">Mot de passe oublié ?</a>
                     <input type="submit" name="connexionForm" value="Se connecter">
                 </form>
